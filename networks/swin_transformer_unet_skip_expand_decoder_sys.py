6521a03b511212406f65c9c2f7d085ca45138174
Ziheng Wang <52435819+azusakou@users.noreply.github.com>
2022-01-10 01:23:40 +0900

    Update swin_transformer_unet_skip_expand_decoder_sys.py

--- networks/swin_transformer_unet_skip_expand_decoder_sys.py
+++ networks/swin_transformer_unet_skip_expand_decoder_sys.py
@@ -3,100 +3,19 @@ import torch.nn as nn
 import torch.utils.checkpoint as checkpoint
 from einops import rearrange
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-#from networks.superpixel.modelssp.Spixel_single_layer import SpixelNet
-#from networks.superpixel import modelssp
-#from networks.legomodel.attention.ExternalAttention import ExternalAttention # TODO add new
-#from networks.legomodel.attention.SKAttention import SKAttention
+
+
+
 
 from torch.nn.init import kaiming_normal_, constant_
-from .superpixel.modelssp.model_util import *
-from .superpixel.train_util import *
+
 from .cct import ChannelTransformer, get_CTranS_config
 from networks.legomodel.attention.ExternalAttention import ExternalAttention
 config_vit = get_CTranS_config()
 
-class SpixelNet(nn.Module):
-    expansion = 1
-    def __init__(self, input_channel=3, batchNorm=True):
-        super(SpixelNet, self).__init__()
+from skimage.segmentation import slic
+from skimage.segmentation import mark_boundaries
 
-        self.batchNorm = batchNorm
-        self.assign_ch = 9 # TODO for 2D data, use 2
-
-        self.conv0a = conv(self.batchNorm, input_channel, input_channel * 2, kernel_size=3)
-        self.conv0b = conv(self.batchNorm, input_channel * 2, input_channel * 2, kernel_size=3)
-
-        self.conv1a = conv(self.batchNorm, input_channel * 2, input_channel * 4, kernel_size=3, stride=2)
-        self.conv1b = conv(self.batchNorm, input_channel * 4, input_channel * 4, kernel_size=3)
-
-        self.conv2a = conv(self.batchNorm, input_channel * 4, input_channel * 8, kernel_size=3, stride=2)
-        self.conv2b = conv(self.batchNorm, input_channel * 8, input_channel * 8, kernel_size=3)
-
-        self.conv3a = conv(self.batchNorm, input_channel * 8, input_channel * 16, kernel_size=3, stride=2)
-        self.conv3b = conv(self.batchNorm, input_channel * 16, input_channel * 16, kernel_size=3)
-
-        self.conv4a = conv(self.batchNorm, input_channel * 16, input_channel * 32, kernel_size=3, stride=2)
-        self.conv4b = conv(self.batchNorm, input_channel * 32, input_channel * 32, kernel_size=3)
-
-        self.deconv3 = deconv(input_channel * 32, input_channel * 16)
-        self.conv3_1 = conv(self.batchNorm, input_channel * 32, input_channel * 16)
-        self.pred_mask3 = predict_mask(input_channel * 16, self.assign_ch)
-
-        self.deconv2 = deconv(input_channel * 16, input_channel * 8)
-        self.conv2_1 = conv(self.batchNorm, input_channel * 16, input_channel * 8)
-        self.pred_mask2 = predict_mask(64, self.assign_ch)
-
-        self.deconv1 = deconv(input_channel * 8, input_channel * 4)
-        self.conv1_1 = conv(self.batchNorm, input_channel * 8, input_channel * 4)
-        self.pred_mask1 = predict_mask(input_channel * 4, self.assign_ch)
-
-        self.deconv0 = deconv(input_channel * 4, input_channel * 2)
-        self.conv0_1 = conv(self.batchNorm, input_channel * 4, input_channel * 2)
-        self.pred_mask0 = predict_mask(input_channel * 2, self.assign_ch)
-
-        self.convxa = nn.Conv2d(self.assign_ch, input_channel, kernel_size=3, stride=1, padding=1)
-        self.softmax = nn.Softmax(1)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-                kaiming_normal_(m.weight, 0.1)
-                if m.bias is not None:
-                    constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                constant_(m.weight, 1)
-                constant_(m.bias, 0)
-    def forward(self, x):
-        out1 = self.conv0b(self.conv0a(x))  # 5*5
-        out2 = self.conv1b(self.conv1a(out1))  # 11*11
-
-        out3 = self.conv2b(self.conv2a(out2))  # 23*23
-        out4 = self.conv3b(self.conv3a(out3))  # 47*47
-        out5 = self.conv4b(self.conv4a(out4))  # 95*95
-
-        out_deconv3 = self.deconv3(out5)
-        b1, c1, h1, w1 = out_deconv3.shape
-        b2, c2, h2, w2 = out4.shape
-        if h1 != h2:
-            out_deconv3 = self.convxa(out_deconv3)
-        concat3 = torch.cat((out4, out_deconv3), 1)
-        out_conv3_1 = self.conv3_1(concat3)
-
-        out_deconv2 = self.deconv2(out_conv3_1)
-        concat2 = torch.cat((out3, out_deconv2), 1)
-        out_conv2_1 = self.conv2_1(concat2)
-
-        out_deconv1 = self.deconv1(out_conv2_1)
-        concat1 = torch.cat((out2, out_deconv1), 1)
-        out_conv1_1 = self.conv1_1(concat1)
-
-        out_deconv0 = self.deconv0(out_conv1_1)  # out_conv1_1)
-        concat0 = torch.cat((out1, out_deconv0), 1)
-        out_conv0_1 = self.conv0_1(concat0)
-        mask0 = self.pred_mask0(out_conv0_1)  # TODO try mask0
-        prob0 = self.softmax(mask0)
-        #prob1 = self.convxa(mask0)
-
-        return prob0
 
 class CCA(nn.Module):
     """
@@ -792,14 +711,13 @@ class SwinTransformerSys(nn.Module):
         channeld2 = 192
         channeld3 = 384
 
-        self.cnnt1 = torch.nn.Conv2d(in_channels=3, out_channels=48, kernel_size=3, stride=2, padding=1, dilation=1, groups=1, bias=True, padding_mode='zeros')
-
+        self.cnn4CCT = torch.nn.Conv2d(in_channels=3, out_channels=48, kernel_size=3, stride=2, padding=1, dilation=1, groups=1, bias=True, padding_mode='zeros')
+        self.cnn4supp = torch.nn.Conv2d(in_channels=5, out_channels=2, 
+                           kernel_size=1, stride=1, groups=1)
+        
         self.EA_channeld3 = ExternalAttention(d_model = channeld3, S=8)
         self.EA_channeld2 = ExternalAttention(d_model = channeld2, S=8)
         self.EA_channeld1 = ExternalAttention(d_model = channeld1, S=8)
-
-        self.supp = SpixelNet()
-
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
@@ -880,21 +798,24 @@ class SwinTransformerSys(nn.Module):
         return x
 
     def forward(self, x):
-        CCT_modual = False
-        MCCT_modual = False
+        CCT_module = False
+        MCCT_module = False
         EA_channel = False
         superpixel = False
 
-        d0=self.cnnt1(x)
+        if CCT_module or MCCT_module == True:
+            d0=self.cnn4CCT(x)
+            
         if superpixel == True:
-            x_supp = self.supp(x)
-
+            nimage = [mark_boundaries(i.permute(1,2,0),slic(i.permute(1,2,0), n_segments=100, sigma=10)) for i in x]
+            x_supp = torch.tensor(nimage, dtype=torch.float).permute(0,3,1,2)
+            
         x, [d1, d2, d3, d4] = self.forward_features(x)
 
-        if CCT_modual == True:
+        if CCT_module == True:
             d1, d2, d3, d4, w = self.cct(self.skipshape(d1), self.skipshape(d2), self.skipshape(d3), self.skipshape(d4))
             d1, d2, d3, d4 = self.rev_skipshape(d1),self.rev_skipshape(d2),self.rev_skipshape(d3),self.rev_skipshape(d4)
-        if MCCT_modual == True:
+        if MCCT_module == True:
             d0, d1, d2, d3, w = self.mcct(d0, self.skipshape(d1), self.skipshape(d2), self.skipshape(d3))
             d1, d2, d3 = self.rev_skipshape(d1),self.rev_skipshape(d2),self.rev_skipshape(d3)
         if EA_channel == True:
@@ -906,7 +827,8 @@ class SwinTransformerSys(nn.Module):
         x = self.up_x4(x)
 
         if superpixel == True:
-            x = (x + x_supp)/2
+            x = torch.cat([x, x_supp],1)
+            x = self.cnn4supp(x)
 
         return x
 
